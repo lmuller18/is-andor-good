83ad587e409cf968799054b2e51871b51fc59335
Liam Muller <liam.w.muller@gmail.com>
2022-10-19 22:26:22 -0400

    create blog

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Is Andor Good?";
+export const SITE_DESCRIPTION =
+  "Asking and answering the question: Is Andor Good?";
