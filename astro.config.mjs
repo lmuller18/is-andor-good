0d352c6edb7b815eff351558140c1cc8cc0a3cc5
Liam Muller <liam.w.muller@gmail.com>
2022-10-19 22:57:14 -0400

    update tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,13 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://is-andor-good.com",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind({
+      // Example: Disable injecting a basic `base.css` import on every page.
+      // Useful if you need to define and/or import your own custom `base.css`.
+      config: { applyBaseStyles: false },
+    }),
+  ],
 });
